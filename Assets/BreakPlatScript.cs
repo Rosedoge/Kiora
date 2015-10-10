22556ee3dba620d2b655746a13260972f1dab7ba
Roseluck <cinorflynn@yahoo.com>
2015-10-10 15:37:59 -0400

    1.0 test
    Breakable rock

---
+++ Assets/BreakPlatScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakPlatScript : MonoBehaviour {
+
+	public Sprite[] sprites;
+	float timer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D(Collider2D col){
+		timer += 0.1f;
+		Debug.Log (timer + " is Timer");
+		switch ((int)timer) {
+		case 10:
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[1];
+			break;
+		case 20:
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[2];
+			break;
+		case 30:
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[3];
+			break;
+		case 40:
+			Destroy(this.gameObject);
+			break;
+		}
+
+	}
+}
